b2837df5409388ffec2627db39b7eb4b11dea3f3
AliElgohary <alielgohary35@gmail.com>
2023-06-13 18:10:22 +0300

    prooblem 2

---
+++ index2.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+// Read the file
+const jsonData = fs.readFileSync('problem2.json');
+
+//parse it
+const data = JSON.parse(jsonData);
+
+// Convert the dates to the format
+data.accidents.forEach(accident => {
+  const dateParts = accident.date.split('/');
+  const formattedDate = `${dateParts[2]}-${dateParts[0].padStart(2, '0')}-${dateParts[1].padStart(2, '0')}`;
+  accident.date = formattedDate;
+});
+
+// Write the modified data to result.json
+const result = JSON.stringify(data, null, 2);
+fs.writeFileSync('output2.json', result);
